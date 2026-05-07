26315d4daa153fdda9fdcd26130371d11a78f232
MainulDip <mainuldip@gmail.com>
2026-05-07 16:10:10 -0400

    ownership of vector docs

--- apps/common-collection-105/src/main.rs
+++ apps/common-collection-105/src/main.rs
@@ -1,3 +1,5 @@
+use std::vec;
+
 fn main() {
     println!(" Playing with vector 101 ------------------------- ");
 
@@ -10,7 +12,7 @@ fn main() {
     let mut vector_with_initial_value = vec![1, 2, 3];
     vector_with_initial_value.push(4);
 
-    let third: &i32 = &vector_with_initial_value[2]; // if index is out-of-bound, the program will panic and exit
+    let third: &i32 = &vector_with_initial_value[2]; // if index is out-of-bound, the program will panic and exit, here it isn't
     println!("The third element is {third}");
 
     let option_type_third = vector_with_initial_value.get(2);
@@ -18,4 +20,19 @@ fn main() {
         Some(value) => println!("option_type_third is {value}"),
         None => println!("option_type_third doesn't exists"),
     }
+
+    // vector iteration
+    let mut vec_iter = vec![1, 2, 3];
+    for i in &vec_iter { // here without `&` will also do the loop, but the ownership will be moved
+        println!("i = {i} ");
+    }
+    println!("now the vec_iter = {:?}", vec_iter); // it works as we're using &vec_iter not `vec_iter` directly in the loop
+
+    for i in &mut vec_iter {
+        *i += 10; // we need to deference for further assignment in this case
+        println!("i after transformation is = {i}");
+    }
+    println!("vec_iter still valid as we've not moved the ownership yet, proof = {:?}", vec_iter);
+
+    // We cannot read 
 }
